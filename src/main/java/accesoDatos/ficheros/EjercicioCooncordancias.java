87e0e48803643a25a2aa478ebb3e7d223995ea08
bcarrascosergio <bcarrascosergio@gmail.com>
2026-09-24 18:10:52 +0200

    primera subida

--- src/main/java/accesoDatos/ficheros/EjercicioCooncordancias.java
+++ src/main/java/accesoDatos/ficheros/EjercicioCooncordancias.java
@@ -1,7 +1,18 @@
 package accesoDatos.ficheros;
 
+import java.io.BufferedReader;
+import java.util.Scanner;
+
 public class EjercicioCooncordancias {
     static void main(String[] args) {
+
+        String aficiones;
+        BufferedReader br = null;
+
+        Scanner entrada = new Scanner(System.in);
+        
+
+
 
 
 
